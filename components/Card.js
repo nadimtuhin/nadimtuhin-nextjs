c9a9e3c8c7e9a563ee069d9a44ef5d8937851579
Omar Faruque Tuhin <nadimtuhin@gmail.com>
2024-12-22 00:12:54 +0600

    Refactor Card Component Styles for Improved Readability
    - Adjusted className formatting in Card.js for better clarity and consistency.
    - Enhanced the layout structure by modifying the order of utility classes.

--- components/Card.js
+++ components/Card.js
@@ -6,7 +6,7 @@ const Card = ({ title, description, imgSrc, href }) => (
     <div
       className={`${
         imgSrc && 'h-full'
-      }  overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
+      } border-opacity-60·overflow-hidden·rounded-md·border-2·border-gray-20`}
     >
       {imgSrc &&
         (href ? (
