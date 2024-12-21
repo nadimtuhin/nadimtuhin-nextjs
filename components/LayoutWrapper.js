e4b869839db242016f2a89d72c666834d8a28abd
Omar Faruque Tuhin <nadimtuhin@gmail.com>
2024-12-22 00:26:59 +0600

    Refactor LayoutWrapper to Simplify Header Title Display
    - Removed unnecessary conditional class for header title visibility in LayoutWrapper component.
    - Ensured consistent styling by maintaining the header title display without hiding it on smaller screens.

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -19,9 +19,7 @@ const LayoutWrapper = ({ children }) => {
                   <Logo />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
-                    {siteMetadata.headerTitle}
-                  </div>
+                  <div className="h-6 text-2xl font-semibold">{siteMetadata.headerTitle}</div>
                 ) : (
                   siteMetadata.headerTitle
                 )}
